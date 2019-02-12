4a30876dc695572b470647fdd29d98b2c545fca9
unknown <chasel>
2019-02-12 17:09:59 +0800

    simple factory upload

---
+++ 简单工厂方法之解析HTTP文件.py
@@ -0,0 +1,48 @@
+'''
+    此类的关键作用是配合简单工厂类处理HTTP FTP 上的文件
+    因为之前的解析文件类里面写的是路径 所以这块我们先行下载文件
+    DownloadFile 是父类 里面3个方法
+        init 主要是传入和初始化需要的变量
+        download 下载文件
+        return_path 返回文件路径
+
+'''
+import os
+import requests
+
+
+class DownloadFile(object):
+    def __init__(self, url):
+        self.url = url
+        self.save_path = './files'
+        self.path = ''
+
+    def download(self):
+        pass
+
+    def return_path(self):
+        self.download()
+        return self.path
+
+
+class DownloadHttpFile(DownloadFile):
+
+    def download(self):
+
+        try:
+            f = open(self.save_path + os.sep + self.url.split('/')[-1], 'w', encoding='utf8')
+            f.write(requests.get(url=self.url).content.decode())
+            f.close()
+            self.path = self.save_path + os.sep + self.url.split('/')[-1]
+        except FileOpenFail as e:
+            raise e
+
+
+class FileOpenFail(Exception):
+    pass
+
+
+
+if __name__ == '__main__':
+    download_api = DownloadHttpFile(url='https://yirenfang.app/file.json')
+    print(download_api.return_path())
---
+++ 简单工厂方法之解析文件.py
@@ -0,0 +1,50 @@
+'''
+    使用简单工厂方法实现解析 xml文件以及json文件
+'''
+import json
+import xml.etree.ElementTree as etree
+import os
+
+from 简单工厂方法之解析HTTP文件 import DownloadHttpFile
+
+
+class Parser():
+    def __init__(self, path):
+        self.f = open(path, 'r', encoding='utf8')
+
+    def __del__(self):
+        self.f.close()
+
+    def parse_data(self):
+        pass
+
+
+class JsonParser(Parser):
+    def parse_data(self):
+        return json.load(self.f)
+
+
+class XmlParser(Parser):
+    def parse_data(self):
+        return etree.parse(self.f)
+
+
+
+def connect_factory(path):
+    handlers = {
+        '.json': JsonParser,
+        '.xml': XmlParser,
+    }
+    return handlers.get(os.path.splitext(path)[-1], lambda x: None)(path)
+
+def if_http_ftp(url):
+    if url.startswith('http') or url.startswith('http'):
+        return connect_factory(path=DownloadHttpFile(url=url).return_path())
+    else:
+        return connect_factory(path=url)
+
+
+if __name__ == '__main__':
+    conn_api = if_http_ftp('https://yirenfang.app/file.json')
+    data = conn_api.parse_data()
+    print(data)
